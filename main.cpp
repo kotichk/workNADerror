84474256569dc400c4e82c12cbc90d0f232d6e0c
So <lps.2019@mail.ru>
2026-02-25 22:25:41 +0500

    end work

--- main.cpp
+++ main.cpp
@@ -27,6 +27,22 @@ struct LogEntry {
     string message;
 };
 
+void InStruct(const string& line, LogEntry& entr)
+{
+    stringstream ss(line);
+    char charr;
+    ss >> entr.year >> charr >> entr.month >> charr >> entr.day;
+    ss >> entr.hour >> charr >> entr.minut >> charr >> entr.sec >> charr >> entr.milisec;
+    ss >> entr.cod;
+    ss >> entr.info;
+    ss >> entr.mainThread;
+    ss >> entr.namePotoka;
+    getline(ss, entr.message);
+    if (entr.message.length() > 0 && entr.message[0] == ' ') {
+        entr.message = entr.message.substr(1);
+    }
+}
+
 int main()
 {
     ifstream file("Box-1.17.120.2022-10-31.log");
@@ -53,12 +69,28 @@ int main()
     }
     int index = 0;
     while (getline(file, line) && index < countLine) {
-        logs[index].message = line;
+        InStruct(line, logs[index]);
         index++;
     }
     file.close();
 
-
+    int infomess = 0;
+    int warnmess = 0;
+    int errormess = 0;
+    for (int i = 0; i < countLine; i++) {
+        if (logs[i].info == "INFO") {
+            infomess++;
+        }
+        if (logs[i].info == "WARNING") {
+            warnmess++;
+        }
+        if (logs[i].info == "ERROR") {
+            errormess++;
+        }
+    }
+    cout << "INFO messages: " << infomess << endl;
+    cout << "WARNING messages: " << warnmess << endl;
+    cout << "ERROR messages: " << errormess << endl;
 }
 
 //2022-10-31 21:31:51.448 3260 INFO    MainThread           windows_sync_app_dele Launching Box
